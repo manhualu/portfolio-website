718fc8905e1dc213efcf8e33fcbcf14d36b9db9f
manhualu <manhua.lu64@gmail.com>
2023-01-17 17:16:21 +1100

    Set up nav bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className="flex">
+        <nav>
+          <ul>
+            <div>
+              <li>Manhua</li>
+              <li>Home</li>
+              <li>About</li>
+              <li>Projects</li>
+            </div>
+          </ul>
+        </nav>
+      </div>
+      <Component {...pageProps} />
+    </>
+  );
 }
