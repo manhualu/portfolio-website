5ad2199309eda98fb655fc8c05d27d3254dc5a86
manhualu <manhua.lu64@gmail.com>
2023-01-21 01:54:16 +1100

    Fixed no default export error

--- pages/Footer.tsx
+++ pages/Footer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export const Footer = () => {
+const Footer = () => {
   return (
     <div className="flex justify-center bg-sky-12 p-4">
       <span className="text-md text-gray-10">Made by Manhua Lu 🧡</span>
     </div>
   );
 };
+
+export default Footer;
